6452b5c8c49f36675a52d2f312249dfc8b3db451
asimsaeed <asim.saeed1@yahoo.com>
2024-03-20 07:30:19 -0700

    first push my calculatort

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/ env node
+import inquirer from "inquirer";
+
+const  answer = await inquirer.prompt([
+ {massage: "Enter first number", type: "number", name: "firstNumber" },
+ {massage: "Enter Second number", type: "number", name: "secondnumber" },
+ {massage: "select one of the operator to perform operation",
+ type: "list", name: "operator",
+ choices: ["Addition", "subtraction", "multiplication", "division"],
+},
+]);
+console.log(answer)
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondnumber )}
+else if(answer.operator === "subtraction") {
+    console.log(answer.firstNumber - answer.secondnumber )}
+else if(answer.operator === "multiplication") {
+    console.log(answer.firstNumber * answer.secondnumber )}
+else if(answer.operator === "division") {
+    console.log(answer.firstNumber / answer.secondnumber )}
+    
