f88e9947ce32b7b71ee5a98d52e85e82ea6150b0
asimsaeed <asim.saeed1@yahoo.com>
2024-03-20 07:45:10 -0700

    some chages in text

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@
 import inquirer from "inquirer";
 
 const  answer = await inquirer.prompt([
- {massage: "Enter first number", type: "number", name: "firstNumber" },
- {massage: "Enter Second number", type: "number", name: "secondnumber" },
+ {massage: "Enter your first number", type: "number", name: "firstNumber" },
+ {massage: "Enter  your Second number", type: "number", name: "secondnumber" },
  {massage: "select one of the operator to perform operation",
  type: "list", name: "operator",
  choices: ["Addition", "subtraction", "multiplication", "division"],
@@ -18,4 +18,4 @@ else if(answer.operator === "multiplication") {
     console.log(answer.firstNumber * answer.secondnumber )}
 else if(answer.operator === "division") {
     console.log(answer.firstNumber / answer.secondnumber )}
-    
+console.log("end")
